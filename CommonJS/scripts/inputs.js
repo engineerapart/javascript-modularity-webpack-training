48a6c0822a9749bb6299dc2bfaddc875eab947d7
CodinRonan <patrick@wizeline.com>
2016-11-09 14:17:52 -0600

    Added RequireJS

---
+++ CommonJS/scripts/inputs.js
@@ -0,0 +1,19 @@
+function OnKeyPressCounter (event, counter) {
+  var chCode = ('charCode' in event) ? event.charCode : event.keyCode;
+
+  if (chCode == 43 /* + */) {
+    if (counter.selectedIndex < counter.options.length - 1) {
+      counter.selectedIndex++;
+    }
+  }
+  if (chCode == 45 /* - */) {
+    if (counter.selectedIndex > 0) {
+      counter.selectedIndex--;
+    }
+  }
+}
+
+function changeText(event, obj) {
+  var echoEl = document.getElementById('echo-box');
+  echoEl.innerText = obj.value + event.key;
+}
